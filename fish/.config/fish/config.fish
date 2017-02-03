922020ad0ccb2c5ff6d4bc707a3752c41ebddc97
David Winter <i@djw.me>
2017-02-03 09:46:30 +0000

    Fish changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,4 @@
+function json; python -m json.tool; end
 function h; hg $argv; end
 
 function g; git $argv; end
@@ -11,6 +12,9 @@ function prepend_to_path -d "Prepend the given dir to PATH if it exists and is n
     end
 end
 
+prepend_to_path "/Users/davidwinter/bin/gradle/bin"
+prepend_to_path "/Users/davidwinter/bin/maven/bin"
+prepend_to_path "/Users/davidwinter/.composer/vendor/bin"
 prepend_to_path "/Users/davidwinter/Dropbox/bin"
 prepend_to_path "/Users/davidwinter/bin"
 prepend_to_path "$HOME/.rbenv/bin"
@@ -20,13 +24,21 @@ prepend_to_path "/usr/local/bin"
 prepend_to_path "/usr/local/sbin"
 
 rbenv rehash >/dev/null ^&1
+jenv rehah 2>/dev/null
 
 set BROWSER open
 
+set -g -x M2_HOME "/Users/davidwinter/bin/maven"
+set -g -x MAVEN_OPTS "-Xms256m -Xmx512m"
+set -g -x M2 "$M2_HOME/bin"
+set -g -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home"
 set -g -x GOPATH "$HOME/go"
 set -g -x EDITOR vim
 set -g -x NODE_PATH '/usr/local/lib/node_modules'
 set -g -x fish_greeting ''
+set -x NVM_DIR ~/.nvm
+
+source ~/.config/fish/nvm-wrapper/nvm.fish
 
 function git_prompt
   if git root >/dev/null 2>&1
@@ -65,11 +77,12 @@ function dcr; docker-compose run --rm --service-ports $argv; end
 function dc; docker-compose $argv; end
 
 function dcu
-  docker-compose run --rm frontend rm /app/tmp/pids/server.pid
-  docker-compose run --rm backend rm /app/tmp/pids/server.pid
+  docker-compose run --rm web rm /app/tmp/pids/server.pid
+  docker-compose run --rm flourish rm /app/tmp/pids/server.pid
+  docker-compose run --rm platform rm /app/tmp/pids/server.pid
   docker-compose up
 end
 
 function dcb; docker-compose build; end
 
-function mole; sshuttle --dns -r mole 0/0; end
+function mole; sshuttle -l 0.0.0.0 --dns -r mole 0/0; end
