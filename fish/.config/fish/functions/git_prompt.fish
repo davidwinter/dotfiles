bcb43e519dabd7514a285e7e9c728c73dd913b03
David Winter <i@djw.me>
2021-09-08 16:58:06 +0100

    fix: update fish syntax for git stdout and stderr redirections

--- fish/.config/fish/functions/git_prompt.fish
+++ fish/.config/fish/functions/git_prompt.fish
@@ -1,9 +1,9 @@
 function git_prompt
-  if git root >/dev/null 2>&1
+  if git root &>/dev/null
     set_color normal
     printf ' on '
     set_color magenta
-    printf '%s' (git currentbranch ^/dev/null)
+    printf '%s' (git currentbranch 2>/dev/null)
     set_color normal
   end
 end
